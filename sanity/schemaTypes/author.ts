8aa0454c70d30b9384fa61d2a266e4fe984b02c9
nini-btw <mohamed01112002@gmail.com>
2025-03-19 10:00:03 +0100

    sanity

---
+++ sanity/schemaTypes/author.ts
@@ -0,0 +1,21 @@
+import { defineField, defineType } from "sanity";
+import { UserIcon } from "lucide-react";
+
+export const author = defineType({
+  name: "author",
+  title: "Author",
+  type: "document",
+  icon: UserIcon,
+  fields: [
+    defineField({ name: "id", type: "number" }),
+    defineField({ name: "username", type: "number" }),
+    defineField({ name: "email", type: "string" }),
+    defineField({ name: "image", type: "url" }),
+    defineField({ name: "bio", type: "text" }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+});
